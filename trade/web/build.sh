d165ca71a6853b8ffc7fca02c70a3d328cf3b175
rodric rabbah <rodric@gmail.com>
2019-08-01 10:15:04 -0400

    Update build.sh

--- trade/web/build.sh
+++ trade/web/build.sh
@@ -1,24 +1,3 @@
-#
-# Nimbella CONFIDENTIAL
-# ---------------------
-#
-#   2018 - present Nimbella Corp
-#   All Rights Reserved.
-#
-# NOTICE:
-#
-# All information contained herein is, and remains the property of
-# Nimbella Corp and its suppliers, if any.  The intellectual and technical
-# concepts contained herein are proprietary to Nimbella Corp and its
-# suppliers and may be covered by U.S. and Foreign Patents, patents
-# in process, and are protected by trade secret or copyright law.
-#
-# Dissemination of this information or reproduction of this material
-# is strictly forbidden unless prior written permission is obtained
-# from Nimbella Corp.
-#
-
 npm install
 npm run build
 cp -r build/* .
-
