e281936cfb322a2671e6baa73c03e1d2a119a65d
Rodric Rabbah <rodric@gmail.com>
2020-10-11 20:14:04 -0400

    Add gradle build.

--- qrcode/packages/default/qr-java/build.sh
+++ qrcode/packages/default/qr-java/build.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
 set -e 
-mvn install
+
+# set to 'mave' to use maven
+# set to 'gradle' to use gradle
+BUILD="maven"
+
+if [ $BUILD == "maven" ]; then
+    mvn install
+    echo target/qr-1.0.0-jar-with-dependencies.jar > .include
+else
+    if [ $BUILD == "gradle" ]; then
+        gradlew jar
+        echo build/libs/qr-java-1.0.jar > .include
+    else
+        echo unknown builder
+        exit -1
+    fi
+fi
